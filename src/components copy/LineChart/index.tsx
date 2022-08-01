00e72be5e616d90d018c26538b47501ed6215fc4
Davydx7 <oludave0511@gmail.com>
2022-08-01 02:22:00 +0100

    netlify fixes

---
+++ src/components copy/LineChart/index.tsx
@@ -0,0 +1,81 @@
+import { Line } from 'react-chartjs-2'; // chart.js library
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartEvent,
+  ActiveElement,
+  Chart,
+  ActiveDataPoint
+} from 'chart.js';
+import { observer } from 'mobx-react-lite';
+import userStore from '../../store/mobX';
+
+const LineChart = observer(() => {
+  // configuring the chart library
+  ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+  const labels = Object.keys(userStore.graphData);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Users',
+        data: userStore.graphData,
+        borderColor: 'rgb(196, 107, 248)',
+        backgroundColor: 'rgba(57, 1, 59, 0.5)',
+        tension: 0.2,
+        borderWidth: 3
+      }
+    ]
+  };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: true,
+    scales: {
+      x: {
+        title: {
+          color: '#cc78e4',
+          display: true,
+          text: 'Ages'
+        }
+      },
+      y: {
+        title: {
+          color: '#cc78e4',
+          display: true,
+          text: 'Number of users'
+        }
+      }
+    },
+    plugins: {
+      legend: {
+        position: 'top' as const
+      },
+      title: {
+        display: true,
+        text: 'Users age distribution (18-60)'
+      }
+    },
+    onClick(event: ChartEvent, elements: ActiveElement[], chart: Chart) {
+      if (elements.length) {
+        const activePoint: ActiveDataPoint = elements[0];
+        userStore.setShowList(true);
+        userStore.setActiveIndex(activePoint.index);
+      } else {
+        userStore.setShowList(false);
+      }
+    }
+  };
+
+  return <Line data={data} options={options} />;
+});
+
+export default LineChart;
